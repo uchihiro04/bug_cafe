8284d46bcf4ef926995c82e6d8770bf1584f6642
uchihiro <tohchi4122@gmail.com>
2022-03-07 14:18:01 +0900

    定数の値を変更、変数名を変更
    定数内のpriceに対する値を整数に変更しました。変数名order1をdrink_menuに、order2をfood_menuに変更しました。

--- cafe.rb
+++ cafe.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
-  { name: 'カフェラテ', price: '400' },
-  { name: 'チャイ', price: '460' },
-  { name: 'エスプレッソ', price: '340' },
-  { name: '緑茶', price: '450' }
+  { name: 'コーヒー', price: 300 },
+  { name: 'カフェラテ', price: 400 },
+  { name: 'チャイ', price: 460 },
+  { name: 'エスプレッソ', price: 340 },
+  { name: '緑茶', price: 450 }
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
-  { name: 'アップルパイ', price: '520' },
-  { name: 'ホットサンド', price: '410' }
+  { name: 'チーズケーキ', price: 470 },
+  { name: 'アップルパイ', price: 520 },
+  { name: 'ホットサンド', price: 410 }
 ].freeze
 
 def take_order(menus)
@@ -25,9 +25,9 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_menu = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+food_menu = take_order(FOODS)
+total = DRINKS[drink_menu][:price] + FOODS[food_menu][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
